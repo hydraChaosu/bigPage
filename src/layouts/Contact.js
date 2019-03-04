32be93024bf43f61b87cbacb56b2e1f1d73d13f7
hydraChaosu <abmallim@gmail.com>
2019-03-04 17:59:45 +0100

    Add font awesome Add and style footer and contacts

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "../styles/Contact.scss";
 const Contact = () => {
-  return <div className="contact">contact</div>;
+  return (
+    <div className="contact">
+      <div className="email">
+        <a href="mailto:Kamil.Sulecki00@gmail.com">
+          <i class="fas fa-at" /> Kamil.Sulecki00@gmail.com
+        </a>
+      </div>
+      <div className="facebook">
+        <a href="https://www.facebook.com/profile.php?id=100004428977533">
+          <i class="fab fa-facebook-f" /> Mój Facebook
+        </a>
+      </div>
+      <div className="linkedin">
+        <a href="https://pl.linkedin.com/">
+          <i class="fab fa-linkedin" /> Mój LinkedIn(uzupełnij)
+        </a>
+      </div>
+      <div className="git">
+        <a href="https://github.com/hydraChaosu">
+          <i class="fab fa-github" /> Mój Github
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "../styles/Footer.scss";
 const Footer = () => {
-  return <footer className="footer">footer</footer>;
+  return (
+    <footer className="footer">
+      <p>Made by Kamil Sulecki &copy; 2019</p>
+    </footer>
+  );
 };
 
 export default Footer;
