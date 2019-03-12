92a4de4cdf3e3e789a1f9bea59d393163f7b6adb
hydraChaosu <abmallim@gmail.com>
2019-03-12 16:20:59 +0100

    Add styles, text content, img for About component

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "../styles/About.scss";
 const About = () => {
-  return <div className="about">about</div>;
+  return (
+    <div className="about">
+      <img src="https://via.placeholder.com/150/0000FF/808080" alt="" />
+
+      <p>I am Kamil Sulecki.</p>
+      <p>I am 26 years old.</p>
+      <p>
+        I have bachelor deegree of management at Nicolaus Copernicus University.
+      </p>
+      <p>
+        I am intrested in games(co-op, board), history, politics, programing,
+        testing, IT indusrty.
+      </p>
+    </div>
+  );
 };
 
 export default About;
